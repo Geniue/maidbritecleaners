f05cb841185cb87b80a773a03a41703fdad4ad4f
vinith <vinithdevadiga7@.com>
2023-07-16 21:57:08 +0530

    new phone added

--- resources/views/app/static_components/callus_banner.blade.php
+++ resources/views/app/static_components/callus_banner.blade.php
@@ -2,8 +2,8 @@
    <section class="callus-section" id="neededH">
        <div class="callus-section-container">
            <h2 style="font-family: latobold;">Call us for a Free Quote</h2>
-           <a class="call-us-banner-num" href="tel:917-905-3058"
-               style="font-family: latobold; background-color: unset !important; color: unset !important;">917-905-3058</a>
+           <a class="call-us-banner-num" href="tel:212-920-2154"
+               style="font-family: latobold; background-color: unset !important; color: unset !important;">212-920-2154</a>
            <p class="call-us-banner-para" style="font-family: latobold;">
                Voted 5-Star Top Commercial and Residential Cleaners in NYC, Brooklyn &
                Queens. Our customer service hours are 9am to 9pm, 7 days a week.
