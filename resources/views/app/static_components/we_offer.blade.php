f9a46bf5f4d991daf23ec1bfec63ab79d714b374
vinith <vinithdevadiga7@.com>
2023-07-29 19:32:44 +0530

    new circled img added to we_offer section

--- resources/views/app/static_components/we_offer.blade.php
+++ resources/views/app/static_components/we_offer.blade.php
@@ -12,34 +12,34 @@
                 <ul>
                     <li>
                         <img class="lozad" alt="Carpet Cleaning Services NYC" height="54" width="54"
-                            data-src="{{ asset('assets/icons/icon-1.png') }}" style />
+                            data-src="{{ asset('assets/images/icons/v1/house_clean.png') }}" style />
                         <a href="{{ route('house_cleaning') }}"> House Cleaning </a>
                     </li>
                     <li>
                         <img class="lozad" alt="Upholstery Cleaning NYC" height="54" width="54"
-                            data-src="{{ asset('assets/icons/icon-3a.png') }}" style />
+                            data-src="{{ asset('assets/images/icons/v1/post_cont.png') }}" style />
                         <a href="{{ route('post_construct') }}">Post Construction Cleaning</a>
                     </li>
                     <li>
                         <img class="lozad" alt="Rug Cleaner NYC" height="54" width="54"
-                            data-src="{{ asset('assets/icons/icon-2a.png') }}" style />
+                            data-src="{{ asset('assets/images/icons/v1/move_in.png') }}" style />
                         <a href="{{ route('move_in') }}">Move in Cleaning Services</a>
                     </li>
                     <li>
                         <img class="lozad" alt="Mattress Cleaning NYC" height="54" width="54"
-                            data-src="{{ asset('assets/icons/upholstery-cleaning-mattress-cleaning-NYC-icon.png') }}"
+                            data-src="{{ asset('assets/images/icons/v1/move_out.png') }}"
                             style />
                         <a href="{{ route('move_out') }}">Move out Cleaning Services</a>
                     </li>
                     <li>
                         <img class="lozad" alt="Oriental and Persian Rug Cleaning NYC" height="54" width="54"
-                            data-src="{{ asset('assets/icons/oriental-rug-cleaning-NYC-icon.png') }}" style />
+                            data-src="{{ asset('assets/images/icons/v1/apart_clean.png') }}" style />
                         <a href="{{ route('apart_cleaning') }}">Apartment Cleaning Services</a>
                     </li>
                     <li>
                         <img rel="canonical" class="lozad" alt="Pet Odor Removal and Pet Urine Removal Services"
                             height="54" width="54"
-                            data-src="{{ asset('assets/icons/carpet-cleaning-pet-stain-pet-odor-removal-service-icon.png') }}"
+                            data-src="{{ asset('assets/images/icons/v1/house_keep.png') }}"
                             style />
                         <a href="{{ route('house_keeping') }}"> House Keeping </a>
                     </li>
@@ -48,37 +48,37 @@
                 <ul>
                     <li>
                         <img rel="canonical" class="lozad" alt="Green Carpet Cleaner NYC" height="54" width="54"
-                            data-src="{{ asset('assets/icons/icon-5a.png') }}" style />
+                            data-src="{{ asset('assets/images/icons/v1/green_clean.png') }}" style />
                         <a href="{{ route('green_cleaning') }}">Green Cleaning</a>
                     </li>
                     <li>
                         <img rel="canonical" class="lozad"
                             alt="Professional Office and Commercial Carpet Cleaning Services" height="54"
-                            width="54" data-src="{{ asset('assets/icons/icon-4a.png') }}" style />
+                            width="54" data-src="{{ asset('assets/images/icons/v1/com_clean.png') }}" style />
                         <a href="{{ route('comm_cleaning') }}">Commercial Cleaning</a>
                     </li>
                     <li>
                         <img rel="canonical" class="lozad" alt="Sofa Cleaning NYC" height="54" width="54"
-                            data-src="{{ asset('assets/icons/couch-cleaning-sofa-cleaning-NYC-icon.png') }}" style />
+                            data-src="{{ asset('assets/images/icons/v1/maid.png') }}" style />
                         <a href="{{ route('maid_cleaning') }}">Maid Cleaning</a>
                     </li>
                     <li>
                         <img rel="canonical" class="lozad" alt="Vehicle and Auto Cleaning" height="54"
-                            width="54" data-src="{{ asset('assets/icons/auto-interior-cleaning-icon.png') }}"
+                            width="54" data-src="{{ asset('assets/images/icons/v1/reside_clean.png') }}"
                             style />
                         <a href="{{ route('res_cleaning') }}"> Residential Cleaning Services </a>
                     </li>
                     <li>
                         <img rel="canonical" class="lozad" alt="Pet Odor Removal and Pet Urine Removal Services"
                             height="54" width="54"
-                            data-src="{{ asset('assets/icons/carpet-cleaning-pet-stain-pet-odor-removal-service-icon.png') }}"
+                            data-src="{{ asset('assets/images/icons/v1/deep_clean.png') }}"
                             style />
                         <a href="{{ route('deep_cleaning') }}"> Deep Cleaning Services </a>
                     </li>
                     <li>
                         <img rel="canonical" class="lozad" alt="Pet Odor Removal and Pet Urine Removal Services"
                             height="54" width="54"
-                            data-src="{{ asset('assets/icons/carpet-cleaning-pet-stain-pet-odor-removal-service-icon.png') }}"
+                            data-src="{{ asset('assets/images/icons/v1/Maid.png') }}"
                             style />
                         <a href="{{ route('cleaning_lady') }}">Cleaning Lady </a>
                     </li>
